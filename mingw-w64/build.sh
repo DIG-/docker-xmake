d043450703bb21b1a6db07fa7fcf4ef5906c2173
Gustavo de Oliveira Silva (DIG) <contato@dig.dev.br>
2024-01-30 09:12:35 -0300

    Added automatic latest version detection

--- mingw-w64/build.sh
+++ mingw-w64/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 UBUNTU=${UBUNTU:-latest}
-XMAKE=${XMAKE:-2.8.6}
+if [ -z "$XMAKE" ]; then
+    LATEST="-t diguser/xmake-mingw-w64:latest"
+    XMAKE=$(curl -fs https://api.github.com/repos/xmake-io/xmake/releases/latest | grep -E "\"tag_name\":\s?\"v[0-9.]+\"" | grep -Eo "[0-9.]+")
+fi
+echo "Build docker with ubuntu:$UBUNTU and xmake:$XMAKE"
 docker build \
     -t diguser/xmake-mingw-w64:$XMAKE \
+    $LATEST \
     --build-arg ubuntu=$UBUNTU \
     --build-arg xmake=$XMAKE \
     --compress \
--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 UBUNTU=${UBUNTU:-latest}
-XMAKE=${XMAKE:-2.8.6}
+if [ -z "$XMAKE" ]; then
+    LATEST="-t diguser/xmake-ubuntu:latest"
+    XMAKE=$(curl -fs https://api.github.com/repos/xmake-io/xmake/releases/latest | grep -E "\"tag_name\":\s?\"v[0-9.]+\"" | grep -Eo "[0-9.]+")
+fi
+echo "Build docker with ubuntu:$UBUNTU and xmake:$XMAKE"
 docker build \
     -t diguser/xmake-ubuntu:$XMAKE \
+    $LATEST \
     --build-arg ubuntu=$UBUNTU \
     --build-arg xmake=$XMAKE \
     --compress \
